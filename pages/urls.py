3bbe80752744637242a20b5bed4b46fe5300b7f2
Micah <mk38712@uga.edu>
2022-07-24 11:36:49 -0400

    updated views 2.5

--- pages/urls.py
+++ pages/urls.py
@@ -4,10 +4,10 @@ from pages import views
 
 
 urlpatterns = [
+    path("results/", views.display_messages, name="messages"),
     re_path(
         r".*/*",
         views.covert_channel,
         name="home",
     ),
-    path("results/", views.display_messages, name="messages"),
 ]
